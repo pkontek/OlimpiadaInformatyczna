368c28c5bfac85fd9a164b2c0b243021330b03b7
Piotr Kontek <piotr.kontek@gmail.com>
2023-12-04 15:01:20 +0100

    Zmiany po zapoznaniu się z omówieniem zadania.

--- 2023/Przyciski/prz.py
+++ 2023/Przyciski/prz.py
@@ -4,14 +4,13 @@ import pathlib
 import itertools
 
 n,m = 0,0
-wierszeZDwomaPrzyciskami = []
-ilePrzyciskowWWierszu = []
-ilePrzyciskowWKolumnie = []
 przyciskiWWierszu = []
 przyciskiWKolumnie = []
 stos = []
 przyciski = []
 V = 0
+graf = dict()
+wierzcholki = set()
 
 def wczytajDaneZPliku(path):
     global n, m
@@ -30,95 +29,81 @@ def wczytajDaneZStdin():
 
     zaladujTablice(lines)
 
-def daSieAktywowacParzyste():
-    '''
-    
-    AKTYWNE PARZYSTE 
-    a. aktywowanie parzystych przycisków będzie możliwe jeśli istnieją przyciski co najmniej 2 przyciski w jednym rzędzie mające swój odpowiednik w drugim rzędzie. Np.:
-    o o -    o - - o    - - - -  
-    - - -    - - - -    - o o -
-    o o -    o - - o    - o o -
-             - - - -    - - - -
-    
-    b. jest jeszcze taki przypadek:
-    - - o o    - - o o
-    - - - -    - - - - 
-    o - o -    o - - o 
-    o - - o    o - o -
-
-    zauważmy, że da się aktywować parzyste jeśli graf zbudowany poprzez połączenie przycisków leżących w jednym wierszu i leżących w jednej kolumnie będzie cykliczny.
-
-    '''
-    graf = dict()
-    
-    for w in przyciskiWWierszu:
-        if not w is None and len(w) > 1:
-            poprz = w[0]
-            for nast in w[1:]:
-                dodajDoGrafu(graf, poprz, nast)
-                poprz = nast
-    for k in przyciskiWKolumnie:
-        if not k is None and len(k) > 1:
-            poprz = k[0]
-            for nast in k[1:]:
-                dodajDoGrafu(graf, poprz, nast)
-                poprz = nast
-    jest = grafMaCykl(graf)
-    wynik = []
-    if jest:
-        poprz = None
-        for p in stos[::-1]:
-            if poprz == None:
-                wynik.append(p)
-            else:
-                if poprz != wynik[-1:][0]:
-                    a = przyciski[p]
-                    b = przyciski[poprz]
-                    c = przyciski[wynik[-1:][0]]
-                    if (b[0] == c[0] and a[0] != c[0]) or (b[1] == c[1] and a[1] != c[1] ):
-                        wynik.append(poprz)
-            poprz = p
-    return jest, wynik
-
-def dodajDoGrafu(graf, prz1, prz2):
-    if prz1 not in graf:
-        graf.update({prz1 : [prz2]})
+def dodajDoGrafu(graf, w1, w2):
+    if w1 not in graf:
+        graf.update({w1 : [w2]})
     else:
-        graf[prz1].append(prz2)
-    if prz2 not in graf:
-        graf.update({prz2 : [prz1]})
+        graf[w1].append(w2)
+    if w2 not in graf:
+        graf.update({w2 : [w1]})
     else:
-        graf[prz2].append(prz1)
+        graf[w2].append(w1)
 
-def grafMaCykl(graf):
-    visited = [False]*(V)
+def zbadajGraf(graf):
+    global ileWierzch, wiersz, kolumna, wierzcholki
+    visited = [False]*(n*2)
+    parzystaIlWierzch = True;
 
-    for i in range(V):
+    wynik = set()
+
+    for i in range(n*2):
         # Don't recur for u if it
         # is already visited
         if visited[i] == False:
-            if(isCyclicUtil(graf, i, visited, -1)) == True:
-                return True
+            ileWierzch = 1
+            wiersz = -1
+            kolumna = -1
+            wierzcholki = set()
 
-    return False
+            if(isCyclicUtil(graf, i, visited, -1)) == True:
+                #jest cykl - czyli da się osiągnąć parzyste - wynik jest na stosie
+                wiersz = -1
+                kolumna = -1
+                wynik = set()
+
+                for i in stos:
+
+                    if i<n:
+                        wiersz = i
+                    else:
+                        kolumna = i-n
+                    if wiersz >=0 and kolumna >=0:
+                        wynik.add((wiersz,kolumna))
+
+                return True, wynik
+            parzystaIlWierzch = parzystaIlWierzch and ileWierzch%2 == 0
+            wynik.update(wierzcholki)
+    #nie znaleziono cyklu, ale jeśli w każdym spójnym podgrafie jest parzysta ilość wierzchołków, to da się osiągnąć nieparzyste
+    return parzystaIlWierzch, wynik
 
 def isCyclicUtil(graf, v, visited, parent):
- 
+    global ileWierzch, wiersz, kolumna, wierzcholki
+
     visited[v] = True
     stos.append(v)
-
+    
+    if v<n:
+        wiersz = v
+    else:
+        kolumna = v-n
+    
+    # if wiersz >= 0 and kolumna >=0:
+    #     print((wiersz,kolumna))
     #jeśli węzła nie ma w grafie - to znaczy że nie ma powiązań z innymi
     if v in graf.keys():
 
         # Recur for all the vertices
         # adjacent to this vertex
         for i in graf[v]:
-
-            # print("i:%s , v: %s"%(i,v))
-
             # If the node is not
             # visited then recurse on it
+            if i<n:
+                wiersz = i
+            else:
+                kolumna = i-n
+            wierzcholki.add((wiersz,kolumna))
             if visited[i] == False:
+                ileWierzch += 1
                 if(isCyclicUtil(graf, i, visited, v)):
                     return True
             # If an adjacent vertex is
@@ -126,55 +111,16 @@ def isCyclicUtil(graf, v, visited, parent):
             # of current vertex,
             # then there is a cycle
             elif parent != i:
+                
                 stos.append(i)
                 return True
     stos.pop()
     return False
-    
-def daSieAktywowacNieparzyste():
-
-    '''
-    AKTYWNE NIEPARZYSTE 
-    tu mamy kilka przypadków, ale jeśli istnieje choć jeden wiersz lub kolumna bez przycisku na pewno nie ma co rozpartywć nieparzystych.
-    1. przekątne
-    
-    o - -    - - o    - o -    o - -
-    - o -    - o -    - - o    - - o
-    - - o    o - -    o - -    - o -
-
-    - o - - 
-    o - - - 
-    - - o - 
-    - - - o
-
-    2. prostopadłe
-    a. dla n nieparzystego
-
-    o o o    o o o    - o -
-    o - -    - o -    o o o
-    o - -    - o -    - o -
-    
-    b. dla n parzystego
-
-    - o o o    o - o o    - o - -
-    o - - -    - o - -    o - o o
-    o - - -    - o - -    - o - -
-    o - - -    - o - -    - o - -
-    '''
-    jest = False
-    '''jeśli w każdej kolumnie i w każdym wierszu jest donajmniej jeden przycisk zawsze da się zrobić nieparzystą ilość'''
-    jest = len(np.argwhere(ilePrzyciskowWWierszu == 0)) == 0 and len(np.argwhere(ilePrzyciskowWKolumnie == 0)) == 0
-    
-    return jest
 
 def zapiszPrzycisk(x,y):
-    global przyciski, ilePrzyciskowWWierszu, ilePrzyciskowWKolumnie, przyciskiWWierszu, przyciskiWKolumnie, V
+    global przyciski, przyciskiWWierszu, przyciskiWKolumnie, V, graf
     
-    # if przyciskiWWierszu[x-1] < 2:
-    ilePrzyciskowWWierszu[x-1] += 1
-    # if przyciskiWKolumnie[y-1] < 2:
-    ilePrzyciskowWKolumnie[y-1] += 1
-
+    
     przyciski.append((x-1, y-1))
     if przyciskiWWierszu[x-1] is None:
         przyciskiWWierszu[x-1] = []
@@ -183,53 +129,45 @@ def zapiszPrzycisk(x,y):
         przyciskiWKolumnie[y-1] = []
     przyciskiWKolumnie[y-1].append(V)
     
+    #wiersze zaczynają się od 0 a kolumny od n
+    dodajDoGrafu(graf, (x-1), n+(y-1))
+
     V += 1
     
 def zaladujTablice(lines):
-    global ilePrzyciskowWWierszu, ilePrzyciskowWKolumnie, przyciskiWWierszu, przyciskiWKolumnie
+    global przyciskiWWierszu, przyciskiWKolumnie
     
     jest = False
-
-    #do zastanowienia czy trzeba zwiększać tą ilość jeśli jest już 2
-    ilePrzyciskowWWierszu = np.zeros((n), np.int32)
-    ilePrzyciskowWKolumnie = np.zeros((n), np.int32)
     
     przyciskiWWierszu = np.empty((n), dtype=list)
     przyciskiWKolumnie = np.empty((n), dtype=list)
     for l in lines:
         x, y  = map(int,l.split(" "))
         zapiszPrzycisk(x,y)
-    wynik = []
-    if not jest:
-        jest, wynik = daSieAktywowacParzyste()
-    
-    if not jest:
-        jest = daSieAktywowacNieparzyste()
+    jest, wynik = zbadajGraf(graf)
     
     if jest:
         print("TAK")
         if wynik != []:
             print(len(wynik))
             for p in wynik:
-                print(p+1, end = " ")
+                print(przyciski.index(p) + 1, end = " ")
+        print()
     else:
         print("NIE")
-    
 
 sys.setrecursionlimit(1000000)
-# print(sys.getrecursionlimit())
                 
 wczytajDaneZStdin()
 # wczytajDaneZPliku("%s/input.txt"%pathlib.Path(__file__).parent.resolve())
 # wczytajDaneZPliku("%s/testy/in/prz25.in"%pathlib.Path(__file__).parent.resolve())
 # wczytajDaneZPliku('2023/tester-oi-main/ocen/in/prz0.in')
+# wczytajDaneZPliku('2023/tester-oi-main/ocen/in/prz1ocen.in')
 # wczytajDaneZPliku('2023/tester-oi-main/ocen/in/prz2ocen.in')
 # wczytajDaneZPliku('2023/tester-oi-main/ocen/in/prz3ocen.in')
 # print("n: %i, m: %i"%(n,m))
 # print(przyciski)
 
 
-# print(arr)
-# print("wolnePasy: %s\nodcinki: %s"%(wolnePasy, odcinki))
 
 
